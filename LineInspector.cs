133530bbd375250a6389a017ded5b9e25373e680
Chiang-Yuan <41962462+Chiang-Yuan@users.noreply.github.com>
2018-12-02 17:13:30 +0800

    Add files via upload

---
+++ LineInspector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows;
+using UnityEditor;
+
+public class LineInspector : MonoBehaviour {
+
+    public GameObject Line;
+    public Plane Paper;
+    
+    // Use this for initialization
+    void Start()
+    {
+        Paper = gameObject.GetComponent<Plane>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(Ray, out hit))
+            {
+                Instantiate(Line, hit.point, Quaternion.identity, transform);
+            }
+        }
+    }
+    
+}
