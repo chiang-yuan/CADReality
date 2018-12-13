5a94e5f1803c859b859350ae145c407cf1a42cfb
EricChiuBird <45687888+EricChiuBird@users.noreply.github.com>
2018-12-13 12:47:55 +0800

    Add files via upload

--- Scripts/LineInspector.cs
+++ Scripts/LineInspector.cs
@@ -13,15 +13,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Windows;
 using UnityEditor;
+using UnityEngine.XR.iOS;
+using UnityEngine.UI;
 
-enum ButtonFlag { ERASER, PENCIL, DESELECT };
+enum ButtonFlag { ERASER, PENCIL, DESELECT, DRAWRED, DRAWBLUE, DRAWGREEN };
 
-public class LineInspector : MonoBehaviour {
-
+public class LineInspector : MonoBehaviour
+{
     public GameObject Line;
-    
+    [Range(0.01f, 0.1f)]
+    float z = 0.1f;
+    [Range(0.01f, 0.1f)]
+    float x = 0.1f;
+    public Slider slider;
+
     private ButtonFlag buttonFlag;
 
     /* --------------------------------------------------
@@ -31,6 +37,27 @@ public class LineInspector : MonoBehaviour {
     public void clickPencilButton()
     {
         buttonFlag = ButtonFlag.PENCIL;
+        //CancelInvoke();
+
+        //InvokeRepeating("Pencil", 0f, 0.01f);
+    }
+    public void clickDrawRedButton()
+    {
+        buttonFlag = ButtonFlag.DRAWRED;
+        //CancelInvoke();
+
+        //InvokeRepeating("Pencil", 0f, 0.01f);
+    }
+    public void clickDrawBlueButton()
+    {
+        buttonFlag = ButtonFlag.DRAWBLUE;
+        //CancelInvoke();
+
+        //InvokeRepeating("Pencil", 0f, 0.01f);
+    }
+    public void clickDrawGreenButton()
+    {
+        buttonFlag = ButtonFlag.DRAWGREEN;
         //CancelInvoke();
 
         //InvokeRepeating("Pencil", 0f, 0.01f);
@@ -56,9 +83,6 @@ public class LineInspector : MonoBehaviour {
             iter.GetComponent<CylinderLine>().setState("INACTIVE");
         }
     }
-
-    
-
     public void Begin()
     {
         // Call TimedUpdate immediately, repeat every 0.01 seconds
@@ -71,33 +95,18 @@ public class LineInspector : MonoBehaviour {
         CancelInvoke();
     }
 
-    void Draw()
-    {
-        Debug.Log("You have clicked the button!");
-
-        if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || (Input.GetMouseButtonDown(0)))
-        {
-            var Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(Ray, out hit))
-            {
-                Instantiate(Line, hit.point, Quaternion.identity, transform);
-            }
-        }
-    }
-
-
     // Use this for initialization
     void Start()
     {
     }
 
-
-
     // Update is called once per frame
     void Update()
     {
-        if (buttonFlag == ButtonFlag.PENCIL) Pencil();
+        if (buttonFlag == ButtonFlag.PENCIL) Pencil(Color.black, slider);
+        if (buttonFlag == ButtonFlag.DRAWRED) Pencil(Color.red, slider);
+        if (buttonFlag == ButtonFlag.DRAWBLUE) Pencil(Color.blue, slider);
+        if (buttonFlag == ButtonFlag.DRAWGREEN) Pencil(Color.green, slider);
         if (buttonFlag == ButtonFlag.ERASER) Eraser();
     }
 
@@ -105,7 +114,7 @@ public class LineInspector : MonoBehaviour {
     * Private Function
     * -------------------------------------------------- */
 
-    private void Pencil()
+    private void Pencil(Color color, Slider mainslider)
     {
         if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
             || (Input.GetMouseButtonDown(0)))
@@ -114,7 +123,9 @@ public class LineInspector : MonoBehaviour {
             RaycastHit hit;
             if (Physics.Raycast(Ray, out hit))
             {
-                Instantiate(Line, hit.point, Quaternion.identity, transform);
+                GameObject newLine = Instantiate(Line, hit.point, Quaternion.identity, transform);
+                newLine.GetComponent<Renderer>().material.color = color;
+                newLine.transform.localScale = new Vector3(mainslider.value, 0.01f, mainslider.value);
             }
         }
     }
@@ -132,5 +143,4 @@ public class LineInspector : MonoBehaviour {
             }
         }
     }
-
 }
